45db91a2b877624e29773f7ec0a55613f0cf34c9
Danny <datlacve2@gmail.com>
2024-06-01 23:43:53 +0700

    update

---
+++ src/main/webapp/public/assets/js/plugins-init/jquery-asColorPicker.init.js
@@ -0,0 +1,15 @@
+(function($) {
+    "use strict"
+
+    // Colorpicker
+    $(".as_colorpicker").asColorPicker();
+    $(".complex-colorpicker").asColorPicker({
+        mode: 'complex'
+    });
+    $(".gradient-colorpicker").asColorPicker({
+        mode: 'gradient',
+        gradient: {
+            switchable: false
+        }
+    });
+})(jQuery);
